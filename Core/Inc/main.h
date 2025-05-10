9dd0cf3f096a2a3fc79e4dc5804f673f59e920f2
Laith <Laithalarmouti@gmail.com>
2025-05-10 04:45:13 +0200

    Initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,18 @@
+/*
+ * main.h
+ *
+ *  Created on: May 8, 2025
+ *      Author: MSI
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#include "stm32f4xx_hal.h"
+
+#define SYS_CLOCK_FREQ_50_MHZ 50
+#define SYS_CLOCK_FREQ_80_MHZ 80
+#define SYS_CLOCK_FREQ_100_MHZ 100
+
+
+#endif /* INC_MAIN_H_ */
